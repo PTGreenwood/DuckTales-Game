b392ecaa6d5f9bc940757490fd813f9183741993
Wian Botha <wianbotha@gmail.com>
2016-08-16 20:41:05 +1000

    Updated Threat manager

--- ducktales/src/main/java/uq/deco2800/ducktales/entities/ThreatManager.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/entities/ThreatManager.java
@@ -11,11 +11,51 @@ import java.util.*;
 
 public class ThreatManager {
  private ArrayList<Effects> EffectsList = new ArrayList<Effects>();
- private ArrayList<Effects> EnemiesList = new ArrayList<Effects>();
+ private ArrayList<Enemies> EnemiesList = new ArrayList<Enemies>();
+ 
+ //Temp variables
+ public Effects tempEffect;
+ public Enemies tempEnemy;
  
  public void ThreatsLVL1(){
+	 //Clear Effects
 	 EffectsList.clear();
 	 EnemiesList.clear();
+	 
+	 //------Effects---------
+	 	//Create Temp variable
+	 	tempEffect = new Effects("Earthqauke");
+	 	//Add parameters to temp variable
+	 	//------------
+	 	//Add temp variable to list
+	 	EffectsList.add(tempEffect);
+	 	
+	 	//Create Temp variable
+	 	tempEffect = new Effects("Poisson Cloud");
+	 	//Add parameters to temp variable
+	 	//------------
+	 	//Add temp variable to list
+	 	EffectsList.add(tempEffect);
+	 	
+	 	
+	 //-------Enemies--------
+	 	//Create Temp variable
+	 	tempEnemy = new Enemies("Deathclaw");
+	 	//Add parameters to temp variable
+	 	//------------
+	 	//Add temp variable to list
+	 	EnemiesList.add(tempEnemy);
+	 	
+	 	//Create Temp variable
+	 	tempEnemy = new Enemies("Cartman");
+	 	//Add parameters to temp variable
+	 	//------------
+	 	//Add temp variable to list
+	 	EnemiesList.add(tempEnemy);
+	 	
+	 	
+	 
+	 
 	 
 	 
  }
@@ -24,14 +64,22 @@ public class ThreatManager {
  
  
  public void ThreatsLVL2(){
-	 
+	//Clear Effects
+	 EffectsList.clear();
+	 EnemiesList.clear();
  }
  
  
  public void ThreatsLVL3(){
+	//Clear Effects
+	 EffectsList.clear();
+	 EnemiesList.clear();
 	 
  }
  public void ThreatsLVL4(){
+	//Clear Effects
+	 EffectsList.clear();
+	 EnemiesList.clear();
 	 
  }
  
