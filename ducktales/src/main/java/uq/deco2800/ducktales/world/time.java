6f470c7b4b6a775b245c88eb6b794f22c4cc1771
Felicia <felicia_dubber@hotmail.com>
2016-08-22 16:53:39 +1000

    Testing push

---
+++ ducktales/src/main/java/uq/deco2800/ducktales/world/time.java
@@ -0,0 +1,36 @@
+package uq.deco2800.ducktales.world;
+import uq.deco2800.ducktales.util.Tickable;
+
+import java.util.Random;
+
+import uq.deco2800.ducktales.entities.EntityManager;
+
+import uq.deco2800.ducktales.entities.agententities.AnimalDuck;
+import uq.deco2800.ducktales.entities.agententities.Peon;
+import uq.deco2800.ducktales.entities.resourceentities.Tree;
+import uq.deco2800.ducktales.entities.worldentities.Box;
+import uq.deco2800.ducktales.entities.worldentities.House;
+import uq.deco2800.ducktales.entities.worldentities.LongBox;
+
+import uq.deco2800.ducktales.entities.worldentities.Sawmill;
+
+import uq.deco2800.ducktales.tiles.ResourceRegister;
+
+import uq.deco2800.ducktales.tiles.Tile;
+import uq.deco2800.ducktales.util.Array2D;
+import uq.deco2800.ducktales.util.Point;
+import uq.deco2800.ducktales.util.Tickable;
+
+/**
+ * Processes in game time
+ * 
+ * @author Team Mallicious Mallard
+ *
+ */
+
+public class time implements Tickable {
+
+	public void tick() {
+		
+	}
+}
