b89c91a2bbf675d22037d6b9a9a1400be05c9e50
Josh Benavides <bricsio.benavides@uqconnect.edu.au>
2016-08-22 11:44:39 +1000

    Created and Updated Animal and AnimalDuck getter and setter methods. Added JavaDoc to Animal and AnimalDuck classes. Created AnimalManager class.

--- ducktales/src/main/java/uq/deco2800/ducktales/entities/agententities/Animal.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/entities/agententities/Animal.java
@@ -6,6 +6,8 @@ import java.util.Random;
 
 /**
  * Base class for all animals.
+ *
+ * @author Josh Benavides
  */
 public abstract class Animal extends AgentEntity {
 
@@ -14,15 +16,15 @@ public abstract class Animal extends AgentEntity {
 	// The location that the animal will move to.
 	protected Point nextLocation;
 	// The animal's state of health.
-	private int health;
+	protected int health;
 	// The animal's state of hunger.
-	private int hunger;
+	protected int hunger;
 	// The animal's state of thirst.
-	private int thirst;
+	protected int thirst;
 	// The animal's movement speed.
-	private double speed;
+	protected double speed;
 	// The image to be rendered.
-	private String image;
+	protected String image;
 	// Determines whether the animal can be killed.
 	private boolean canBeKilled;
 	// Determines whether the animal is out of its zone
@@ -36,7 +38,23 @@ public abstract class Animal extends AgentEntity {
 		this.hunger = hunger;
 		this.thirst = thirst;
 		this.speed = speed;
+		this.currentLocation = point;
 		newNextLocation();
+	}
+
+	/**
+	 * Tick stuff.
+	 */
+	@Override
+	public void tick() {
+		if (point.distance(nextLocation) < speed) {
+			point = nextLocation;
+			newNextLocation();
+		} else {
+			point.moveToward(nextLocation, speed);
+		}
+		calculateRenderingOrderValues();
+		currentLocation = nextLocation;
 	}
 
 	/**
@@ -47,43 +65,104 @@ public abstract class Animal extends AgentEntity {
 		nextLocation = new Point(random.nextDouble() * 20, random.nextDouble() * 20);
 	}
 
+	/**
+	 * The animal's health is decreased when attacked by another entity.
+	 */
+	public void animalIsAttacked() {
+		this.health -= 10; // the health will be decreased by the amount of the peon's attack/strength -> replace 10
+		if (this.getHealth() <= 0) {
+			killAnimal();
+		}
+	}
+
+	/**
+	 * Enables the animal to be killed.
+	 */
+	public void killAnimal() {
+		if(this.getHunger() <= 0 && this.getThirst() <= 0) {
+			this.health = 0;
+		}
+		if(this.getHealth() <= 0) {
+			this.canBeKilled = true; // The animal will be removed in the AnimalManager class.
+		}
+	}
+
+	/**
+	 * Identifies that the animal is out of its zone.
+	 */
+	public void isOutOfZone() {
+		// if animal location is out of zone limit:
+		this.outOfZone = true;
+	}
+
+	// Getter Methods Below
+
+	/**
+	 * Returns the animal's current point on the map.
+	 *
+	 * @return currentLocation
+	 */
 	public Point getCurrentLocation() {
 		return new Point(currentLocation);
 	}
 
+	/**
+	 * Return the animal's next point on the map to move to.
+	 *
+	 * @return nextLocation
+	 */
 	public Point getNextLocation() {
 		return new Point(nextLocation);
 	}
 
+	/**
+	 * Returns the animal's current health.
+	 *
+	 * @return health
+	 */
 	public int getHealth() {
 		return health;
 	}
 
+	/**
+	 * Returns the animal's current state of hunger.
+	 *
+	 * @return hunger
+	 */
 	public int getHunger() {
 		return hunger;
 	}
 
+	/**
+	 * Returns the animal's current state of thirst.
+	 *
+	 * @return thirst
+	 */
 	public int getThirst() {
 		return thirst;
 	}
 
-	public void isOutOfZone() {
-		this.outOfZone = true;
+	/**
+	 * Returns the animal's speed.
+	 *
+	 * @return speed
+	 */
+	public double getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * Returns a value saying whether the animal can be killed.
+	 *
+	 * @return canBeKilled
+	 */
+	public boolean canBeKilled() {
+		return canBeKilled;
 	}
 
 	// ???
 	public void spawnAnimal() {
 		// animals spawned using the world class under world package?
-	}
-
-	@Override
-	public void tick() {
-		if (point.distance(nextLocation) < speed) {
-			point = nextLocation;
-			newNextLocation();
-		} else {
-			point.moveToward(nextLocation, speed);
-		}
-		calculateRenderingOrderValues();
+		// AnimalManager class?
 	}
 }
--- ducktales/src/main/java/uq/deco2800/ducktales/entities/agententities/AnimalDuck.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/entities/agententities/AnimalDuck.java
@@ -1,17 +1,65 @@
 package uq.deco2800.ducktales.entities.agententities;
 
 /**
- * Duck animal that lays eggs.
+ * Defines the animal Duck that lays eggs.
  *
  * @author Josh Benavides
  */
-public class AnimalDuck extends Animal{
+public class AnimalDuck extends Animal {
 
 	private final static String IMAGE = "duck";
+	// Whether the duck can lay eggs.
+	private boolean canLayEggs = false;
+	// Whether the duck can drop feathers.
+	private boolean canDropFeathers = false;
+	// Whether the duck is dead.
+	private boolean isDead = false;
 
+
+	// AnimalDuck attributes will be set in the AnimalManagerClass
 	public AnimalDuck(int x, int y) {
-		super(x, y, 100, 100, 100, 0.05, 1, 1, IMAGE);
+		super(x, y, (int) ((Math.random() + 0.1) * 100), (int) ((Math.random() + 0.1) * 100), (int) ((Math.random() +
+			0.1) * 100), 0.05, 1, 1, IMAGE);
 	}
+
+	/**
+	 * Checks whether the duck can lay eggs. Ducks can only lay eggs if their health, hunger, and thirst meet satisfy
+	 * a certain threshold.
+	 */
+	private void layEggs() {
+		if(this.getHealth() >= 85 && this.getHunger() >= 85 && this.getThirst() >= 85) {
+			this.canLayEggs = true;
+		}
+	}
+
+	/**
+	 * Enables the duck to drop feathers.
+	 */
+	private void DropFeathers() {
+		if(this.isDead() == true) {
+			this.canDropFeathers = true;
+		}
+	}
+
+	// Getter methods below.
+
+	/**
+	 * Returns whether the duck is dead.
+	 * @return isDead;
+	 */
+	private boolean isDead() {
+		return this.isDead;
+	}
+
+	/**
+	 *  Returs whether the duck can lay eggs.
+	 * @return canLayEggs
+	 */
+	private boolean canLayEggs() {
+		return this.canLayEggs;
+	}
+
+
 
 
 }
