354057da98e208420b77441a0811519c4450638b
Nicolas Turner <nturn13@live.com.au>
2016-08-22 13:39:59 +1000

    Added affinity generation to Peon class

--- ducktales/src/main/java/uq/deco2800/ducktales/entities/agententities/Peon.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/entities/agententities/Peon.java
@@ -12,43 +12,62 @@ import uq.deco2800.ducktales.util.Point;
  */
 public class Peon extends AgentEntity {
 
-	private final static String TYPE = "peon";
-	
-	private Point goalPoint;
-	
-	private double speed;
-	
-	private int Health =1000;
+	private static final Random RANDOM = new Random();
 
+	private final static String TYPE = "peon";
+
+	private Point goalPoint;
+
+	private double speed;
+
+	private int Health = 1000;
+
+	// affinity
+	private int strength;
+	private int intelligence;
+
+	// affinity bounds
+	private static final int DEFAULT_MAX = 10;
+	private static final int DEFAULT_MIN = 1;
+
+	/**
+	 * @param x
+	 * @param y
+	 */
 	public Peon(int x, int y) {
 		super(x, y, 1, 1, TYPE);
-		this.speed = 0.05;
+		speed = 0.05;
+		strength = RANDOM.nextInt((DEFAULT_MAX - DEFAULT_MIN) + 1) + DEFAULT_MIN;
+		intelligence = RANDOM.nextInt((DEFAULT_MAX - DEFAULT_MIN) + 1) + DEFAULT_MIN;
 		newGoalPoint();
 	}
-	
-	public void ChangeHealth(int NewValue){
-		if (NewValue >0){
-			this.Health =NewValue;
+
+	/**
+	 * @param NewValue
+	 */
+	public void ChangeHealth(int NewValue) {
+		if (NewValue > 0) {
+			this.Health = NewValue;
 		}
 	}
-	
-	public int GetHealth(){
+
+	public int GetHealth() {
 		return Health;
 	}
 
 	@Override
 	public void tick() {
-		if(point.distance(goalPoint) < speed) {
+		if (point.distance(goalPoint) < speed) {
 			point = goalPoint;
 			newGoalPoint();
 		} else {
 			point.moveToward(goalPoint, speed);
 		}
 		calculateRenderingOrderValues();
-		
+
 	}
-	
-	private void newGoalPoint(){
+
+	private void newGoalPoint() {
 		Random random = new Random();
 		goalPoint = new Point(random.nextDouble() * 20, random.nextDouble() * 20);
 	}
