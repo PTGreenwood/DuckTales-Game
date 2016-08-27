4dd8c3d0a072c7eea0c7193a3a19c96dde40e79a
Peter Greenwood <peter.greenwood92@live.com.au>
2016-08-27 17:22:07 +1000

    Restructuring

--- ducktales/src/main/java/uq/deco2800/ducktales/world/time.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/world/time.java
@@ -1,6 +1,5 @@
 package uq.deco2800.ducktales.world;
 
-
 import uq.deco2800.ducktales.util.Tickable;
 
 public class Time implements Tickable {
--- ducktales/src/test/java/uq/deco2800/ducktales/TimeTest.java
+++ ducktales/src/test/java/uq/deco2800/ducktales/TimeTest.java
@@ -1,18 +1,11 @@
 package uq.deco2800.ducktales;
 
-import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
 import org.junit.Test;
 
-import junit.framework.Assert;
-
 import static org.junit.Assert.assertEquals;
-
-import uq.deco2800.ducktales.entities.Effect;
-import uq.deco2800.ducktales.entities.Enemy;
-import uq.deco2800.ducktales.entities.ThreatManager;
 
 public class TimeTest {
 
