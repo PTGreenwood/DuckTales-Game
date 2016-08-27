dd5a679f6a6ecee69fb2ea965bbb5076ff16ff0e
Gabbi Hodge <gabbi.hodge@gmail.com>
2016-08-27 19:47:59 +1000

    Created constructor methods to access the building to be constructed. Currently contains error method, accessor methods and Constructor method Need to integrate with resources, so that the available wood and stone can be compared to required wood/stone for the selected building. Need to integrate with graphic panel, to select building and location for building.

--- ducktales/src/main/java/uq/deco2800/ducktales/entities/worldentities/Constructor.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/entities/worldentities/Constructor.java
@@ -1,5 +1,6 @@
 package uq.deco2800.ducktales.entities.worldentities;
 
+import uq.deco2800.ducktales.entities.worldentities.*;
 
 /**
  * A Construction Interface to select Buildings.
@@ -10,8 +11,35 @@ package uq.deco2800.ducktales.entities.worldentities;
 
 public class Constructor {
 	
-	public Constructor() {
-		
+	public Constructor(double x, double y) {
+		// Change the following lines:
+		// Change to building clicked from constructor panel
+		WorldEntity toCreate = new Bakery(x, y);
+		// Change to resources available - wood, stone
+		if (getStone(toCreate) <= 10) {
+			if (getWood(toCreate) <= 10) {
+				// Something along the lines of World.add(toCreate) ...
+				getTime(toCreate);
+			}
+			errorBuild("wood", toCreate);
+		}
+		errorBuild("stone", toCreate);
+	}
+	
+	private int getStone(WorldEntity building) {
+		return building.resourcesBuildStone();
+	}
+	
+	private int getWood(WorldEntity building) {
+		return building.resourcesBuildWood();
+	}
+	
+	private int getTime(WorldEntity building) {
+		return building.timeToBuild();
+	}
+	
+	public String errorBuild(String material, WorldEntity building) {
+		return "Not enough " + material + " to build a " + building.toString();
 	}
 	
 	
