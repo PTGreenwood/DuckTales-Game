c94a0f4b229aa4cb050336d0d7fd8cd98e91a4b2
BuildTools <unconfigured@null.spigotmc.org>
2016-08-27 16:50:45 +1000

    Getting our branch up to scratch with Master (removing errors and whatnot after code cleanup). Also added Initial Test class for Time. Yet to add to it though.

---
+++ ducktales/src/test/java/uq/deco2800/ducktales/TimeTest.java
@@ -0,0 +1,43 @@
+package uq.deco2800.ducktales;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+import static org.junit.Assert.assertEquals;
+
+import uq.deco2800.ducktales.entities.Effect;
+import uq.deco2800.ducktales.entities.Enemy;
+import uq.deco2800.ducktales.entities.ThreatManager;
+
+public class TimeTest {
+
+	//Instantiate Here 
+	
+	
+	@Test // For firstTest testing (not sure yet :S)
+	public void firstTest() {
+		
+		//Temp Variables
+
+		//Test Here:
+		//AssertTrue("");
+	}
+	
+	@Test //for secondTest testing (also not sure if we will use it just yet)
+	public void secondTest() {
+		
+		//Temp Variables
+	 	
+		
+		//Test Here:
+	 	//assertTrue("");
+		
+	}
+
+
+}
