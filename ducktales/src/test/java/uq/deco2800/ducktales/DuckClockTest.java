164642d5e0455409eab794edf8f6732560f8ad1e
Peter Greenwood <peter.greenwood92@live.com.au>
2016-08-29 12:44:59 +1000

    TestClasses framework and beginning test Methods

--- ducktales/src/test/java/uq/deco2800/ducktales/DuckClockTest.java
+++ ducktales/src/test/java/uq/deco2800/ducktales/DuckClockTest.java
@@ -4,28 +4,82 @@ import org.junit.Test;
 
 public class DuckClockTest {
 
-	//Instantiate Here 
-	//DuckClock clock = new DuckClock();
-	@Test // For firstTest testing (not sure yet :S)
-	public void firstTest() {
+	/**
+	 * firstTest tests getMinute Method
+	 * after setting DuckClock to a certain time do checks
+	 * to see if that time is the same.
+	 */
+	@Test
+	public void testGetMinute() {
 		
-		//Temp Variables
-
-		//Test Here:
-		//AssertTrue("");
-
+		//Instatiate all clocks to be tested against
+		DuckClock validClock = new DuckClock(5, 30);
+		DuckClock negativeClockMinute = new DuckClock(0, -10);
+		//Test for valid time variables
+		
+		//--- Invalid duckClock instantiation tests ---
+		//Test for negative time variables
+		
+		//Test for out of range top end variables
+		
 	}
 	
-	@Test //for secondTest testing (also not sure if we will use it just yet)
-	public void secondTest() {
+	/**
+	 * secondTest tests getHour Method
+	 * after setting DuckClock to a certain time do checks
+	 * to see if that hour is the same at time of launch 
+	 */
+	@Test
+	public void testGetHour() {
 		
-		//Temp Variables
-	 	
-		
-		//Test Here:
-	 	//assertTrue("");
-		
+		//Instatiate all clocks to be tested against
+		DuckClock validClock = new DuckClock(5, 30);
+		DuckClock negativeClockHour = new DuckClock(-4, 0);
+		//Test for valid time variables
+				
+		//--- Invalid duckClock instantiation tests ---
+		//Test for negative time variables
+			
+		//Test for out of range top end variables
 	}
 
-
+	/**
+	 * thirdTest tests setHour Method
+	 * after setting DuckClock to a certain time do checks
+	 * to see if after updating the clock using setHour Method
+	 * that that clock's Hour becomes what it was set to.
+	 */
+	@Test
+	public void testSetHour() {
+		
+		//Instatiate all clocks to be tested against
+		DuckClock validClock = new DuckClock(5, 30);
+		DuckClock negativeClockHour = new DuckClock(-4, 0);
+		//Test for valid time variables
+				
+		//--- Invalid duckClock instantiation tests ---
+		//Test for negative time variables
+			
+		//Test for out of range top end variables
+	}
+	
+	/**
+	 * fourthTest tests setMinute Method
+	 * after setting DuckClock to a certain time do checks
+	 * to see if after updating the clock using setMinute Method
+	 * that that clock's minutes have changed to the set minute
+	 */
+	@Test
+	public void testSetMinute() {
+		
+		//Instatiate all clocks to be tested against
+		DuckClock validClock = new DuckClock(5, 30);
+		DuckClock negativeClockHour = new DuckClock(-4, 0);
+		//Test for valid time variables
+				
+		//--- Invalid duckClock instantiation tests ---
+		//Test for negative time variables
+			
+		//Test for out of range top end variables
+	}
 }
