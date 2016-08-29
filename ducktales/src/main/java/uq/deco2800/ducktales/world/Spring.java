2f2592217ea864e8cc63e2a14213fe0a22430f4a
Hui Ting Ko <huitingko@gmail.com>
2016-08-29 17:14:05 +1000

    adding Spring class and SeasonControlloe class

---
+++ ducktales/src/main/java/uq/deco2800/ducktales/world/Spring.java
@@ -0,0 +1,13 @@
+package uq.deco2800.ducktales.world;
+
+/*
+ * This is the class under the SeasonControllor, this will
+ * show all the features of Spring
+ */
+public class Spring extends SeasonControllor{
+
+	//Instantiate a the environment variables
+	int humidity;
+	int temperature;	
+	
+}
