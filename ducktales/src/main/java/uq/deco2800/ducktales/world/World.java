01042c98dca89d3a0cffdb3a202da4d670f71921
Andrew Georgiou <gcgqldag@gmail.com>
2016-08-30 14:35:53 +1000

    Changed ResourceEntity to be a subclass of WorldEntity as it WorldEntity has pre-existing methods for adding and rendering. Added some Resource methods to ResourceEntity

--- ducktales/src/main/java/uq/deco2800/ducktales/world/World.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/world/World.java
@@ -7,6 +7,7 @@ import uq.deco2800.ducktales.entities.EntityManager;
 
 import uq.deco2800.ducktales.entities.agententities.*;
 import uq.deco2800.ducktales.entities.worldentities.Box;
+import uq.deco2800.ducktales.entities.resourceentities.*;
 
 import uq.deco2800.ducktales.resources.ResourceRegister;
 
@@ -70,6 +71,7 @@ public class World implements Tickable {
 
 //		addEntity(new Duck(10,10));
 		addEntity(new Cow(8, 10));
+		addEntity(new Tree(5, 10));
 	}
 
 	/**
@@ -150,9 +152,8 @@ public class World implements Tickable {
 
 	public void addEntity(AgentEntity entity) {
 		entityManager.addEntity(entity);
-
 	}
-
+	
 	public void addEntity(WorldEntity entity) {
 		int entityX = (int) entity.getX();
 		int entityY = (int) entity.getY();
