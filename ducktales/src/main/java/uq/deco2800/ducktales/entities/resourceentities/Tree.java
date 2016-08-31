2b5c1147c7800f2569e4b6fd816ad428c773c126
Andrew Georgiou <gcgqldag@gmail.com>
2016-08-31 12:25:53 +1000

    Added rare spawn functionality to Tree class. Now has a 5% chance to spawn with double value

--- ducktales/src/main/java/uq/deco2800/ducktales/entities/resourceentities/Tree.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/entities/resourceentities/Tree.java
@@ -7,23 +7,53 @@ import uq.deco2800.ducktales.entities.worldentities.WorldEntity;
 import uq.deco2800.ducktales.resources.ResourceType;
 
 public class Tree extends ResourceEntity{
-	
 	private static ResourceType[] TYPES = {TREE_1, TREE_2, TREE_3};
+	private static long _firstCallTimeSeconds = 0;
+	static Random randomGenerator = new Random();
+	static int randomInt = randomGenerator.nextInt(100);
 	
-	private int stage;
 
-	public Tree(double x, double y) {
-		super(x, y, 1, 1, TYPES[0], value);
-		this.stage = 0;
+	public Tree(double x, double y) {		
+		super(x, y, 1, 1, rare(randomInt, TYPES), value);
+	}
+	
+
+	private static ResourceType rare(int randomInt, ResourceType[] TYPES){
+		if(randomInt < 5){
+			value = 200;
+			return TYPES[1];
+		} else{
+			value = 100;
+			return TYPES[0];
+		}
 	}
 
 	@Override
-	public void tick() {		
+	public void tick() {
+		System.out.println(value);
+		/*long TICKS_AT_EPOCH = 621355968000000000L; 
+		long tick = System.currentTimeMillis()*10000 + TICKS_AT_EPOCH;
+		if(tick%10 == 0){
+			value += 5;
+		}
+		int x = 0;
+		if(tickCountInSeconds()%60 == 0 && x == 0){
+			value += 5;
+			x = 1;
+		}*/
 	}
+	
+	/*private synchronized static int tickCountInSeconds() {
+	      long currentTimeSeconds = System.currentTimeMillis() / 1000L;
+	      if (_firstCallTimeSeconds == 0) {
+	         _firstCallTimeSeconds = currentTimeSeconds;
+	      }
+	      
+	      return (int)(currentTimeSeconds - _firstCallTimeSeconds);
+	   }*/
 
 	@Override
 	public boolean isPassable() {
 		return false;
 	}
-
 }
