60570052a48dc73ed8dc48407fa208c68773fc68
Gabbi Hodge <gabbi.hodge@gmail.com>
2016-09-02 08:18:29 +1000

    Updated javadoc and nested comments. All building classes should have complete commenting. Should have removed the code smells to do with lack of nested comments for tick() method

--- ducktales/src/main/java/uq/deco2800/ducktales/entities/worldentities/Barn.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/entities/worldentities/Barn.java
@@ -14,20 +14,35 @@ public class Barn extends WorldEntity {
 	private static final ResourceType TYPE = ResourceType.BARN;
 	
 	
-	// Initiates a new Barn, and sets the required instance variables
-	// for a barn
+	/**
+	 * Initialise a new barn. Requires the location of the barn
+	 *  to be passed.
+	 * @param x, x location of the building
+	 * @param y, y location of the building
+	 */
 	public Barn(double x, double y) {
 		super(x, y, 2, 2, TYPE);
 		
 	}
 	
+	/**
+	 * Update the WorldEntity properties with those of a barn.
+	 */
 	protected void specifications() {
 		WorldEntity.STONERESOURCES = 4;
 		WorldEntity.WOODRESOURCES = 10;
 		WorldEntity.TIME = 9;
 	}
-
+	
+	/**
+	 * Method to update bakery at each discrete simulation step.
+	 * 
+	 * Note sure if any implementation will be used. To be determined later
+	 * May implement for only some of the classes (hence left in the individual 
+	 * buildings class files).
+	 */
 	@Override
 	public void tick() {
+		// To be implemented if there is to be animation of construction
 	}
 }
