8edc803d77e453dd155d3a7c8bf09d69b395312f
Andrew Georgiou <gcgqldag@gmail.com>
2016-09-03 11:32:59 +1000

    Trees can now grow and increase their value over time.

--- ducktales/src/main/java/uq/deco2800/ducktales/entities/resourceentities/Tree.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/entities/resourceentities/Tree.java
@@ -1,22 +1,34 @@
 package uq.deco2800.ducktales.entities.resourceentities;
 
-import java.util.Random;
-import static uq.deco2800.ducktales.resources.ResourceType.*;
+import java.util.*;
+import java.util.concurrent.*;
 
-import uq.deco2800.ducktales.entities.worldentities.WorldEntity;
+import static uq.deco2800.ducktales.resources.ResourceType.*;
 import uq.deco2800.ducktales.resources.ResourceType;
 
-public class Tree extends ResourceEntity{
+public class Tree extends ResourceEntity implements Runnable{
 	private static ResourceType[] TYPES = {TREE_1, TREE_2, TREE_3};
-	private static long _firstCallTimeSeconds = 0;
 	static Random randomGenerator = new Random();
 	static int randomInt = randomGenerator.nextInt(100);
-	
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
 	public Tree(double x, double y) {		
 		super(x, y, 1, 1, rare(randomInt, TYPES), value);
+		 scheduler.scheduleAtFixedRate(createRunnable(this), 60, 60, TimeUnit.SECONDS);
 	}
 	
+	private Runnable createRunnable(final Tree tree){	
+		Runnable aRunnable = new Runnable(){
+			public void run(){
+				grow();
+			}
+		};
+		return aRunnable;
+	}
+	
+	private void grow(){
+		this.increaseValue(10);
+	}
 
 	private static ResourceType rare(int randomInt, ResourceType[] TYPES){
 		if(randomInt < 5){
@@ -27,31 +39,17 @@ public class Tree extends ResourceEntity{
 			return TYPES[0];
 		}
 	}
+	
+	@Override
+	public void run() {
+		
+	}
 
 	@Override
 	public void tick() {
 		System.out.println(value);
-		/*long TICKS_AT_EPOCH = 621355968000000000L; 
-		long tick = System.currentTimeMillis()*10000 + TICKS_AT_EPOCH;
-		if(tick%10 == 0){
-			value += 5;
-		}
-		int x = 0;
-		if(tickCountInSeconds()%60 == 0 && x == 0){
-			value += 5;
-			x = 1;
-		}*/
 	}
 	
-	/*private synchronized static int tickCountInSeconds() {
-	      long currentTimeSeconds = System.currentTimeMillis() / 1000L;
-	      if (_firstCallTimeSeconds == 0) {
-	         _firstCallTimeSeconds = currentTimeSeconds;
-	      }
-	      
-	      return (int)(currentTimeSeconds - _firstCallTimeSeconds);
-	   }*/
-
 	@Override
 	public boolean isPassable() {
 		return false;
