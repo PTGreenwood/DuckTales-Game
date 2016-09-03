7fb192f0f8ac61057811710372313cfec25ed04a
acxpher <acxpher@gmail.com>
2016-09-03 17:03:32 +1000

    create templates for other god files

--- ducktales/src/main/java/uq/deco2800/ducktales/duckgods/LightGod.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/duckgods/LightGod.java
@@ -1,10 +1,44 @@
 package uq.deco2800.ducktales.duckgods;
 
+import javax.swing.ImageIcon;
+
 import uq.deco2800.ducktales.duckgods.DuckGods;
 
 /**
- * Individual classes for the Light God
  * 
- * @author Taylor Rose
+ * 
+ * @author Freddy
  * 
  */
+public class LightGod extends DuckGods {
+	
+	private boolean isWorshiped =false;
+	private boolean isSelected = false;
+	private boolean noConflict = true;
+	  
+	
+	 public LightGod(String name, String bonus, ImageIcon image) {
+    	super();
+    }
+     public boolean isbuilt(){
+    	
+    		return true;
+    	}
+    
+    public boolean isWorshiped () {
+    	
+		//if(isbuilt=true)
+		{
+    		return true;}}
+    		
+	public void giveaward(){
+		
+		//while 
+	}
+    public void givepenalty()
+    {
+    	/
+    	
+		}  
+        
+}
