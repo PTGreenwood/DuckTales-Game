b691ddd69da161cf3cff491c648e4f8ba189b375
Peter Greenwood <peter.greenwood92@live.com.au>
2016-09-04 23:29:55 +1000

    Fix issue causing build to fail on our branch :S not sure why it did though.

--- ducktales/src/main/java/uq/deco2800/ducktales/world/GameTimeException.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/world/GameTimeException.java
@@ -1,7 +1,5 @@
 package uq.deco2800.ducktales.world;
 
-import org.junit.rules.ExpectedException;
-
 public class GameTimeException extends RuntimeException {
       //Parameterless Constructor
       public GameTimeException() {
