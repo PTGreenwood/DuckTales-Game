b03ef94cb5c76f1ab11232879a429aeb6366463e
Gabbi Hodge <gabbi.hodge@gmail.com>
2016-09-05 07:42:22 +1000

    Added tests to EntityTest.java

--- ducktales/src/test/java/uq/deco2800/ducktales/entities/EntityTest.java
+++ ducktales/src/test/java/uq/deco2800/ducktales/entities/EntityTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import uq.deco2800.ducktales.entities.Entity;
 import uq.deco2800.ducktales.entities.worldentities.Box;
+import uq.deco2800.ducktales.entities.worldentities.House;
 import uq.deco2800.ducktales.entities.worldentities.LongBox;
 
 public class EntityTest {
@@ -183,5 +184,56 @@ public class EntityTest {
 		assertTrue("Entity 1 incorrect!", foreground.compareTo(background) == 1);
 		assertTrue("forground2 incorrect", foreground2.compareTo(foreground) == 0);
 		assertTrue("Entity 2 incorrect!", background.compareTo(foreground) == -1);
+	}
+	
+	/**
+	 * Test hashCode method, to be updated when the hashcode method is also 
+	 * updated.
+	 */
+	@Test
+	public void hashCodeTest() {
+		Entity box1 = new Box(5, 4);
+				
+		assertTrue("box1 incorrect!", box1.hashCode() == 4);
+	}
+	
+	/**
+	 * Test getX and getY methods
+	 */
+	@Test
+	public void getXgetYTest() {
+		Entity box = new Box(5, 4);
+		Entity background = new LongBox(4, 3);
+			
+		assertTrue("box incorrect!", box.getX() == 5);
+		assertTrue("background incorrect", background.getX() == 4);
+		assertTrue("box incorrect!", box.getY() == 4);
+		assertTrue("background incorrect", background.getY() == 3);
+	}
+	
+	/**
+	 * Test getX and getY methods
+	 */
+	@Test
+	public void getXLengthgetYLengthTest() {
+		Entity box = new Box(5, 4);
+		Entity background = new LongBox(4, 3);
+			
+		assertTrue("box incorrect!", box.getXLength() == 1);
+		assertTrue("background incorrect", background.getXLength() == 2);
+		assertTrue("box incorrect!", box.getYLength() == 1);
+		assertTrue("background incorrect", background.getYLength() == 1);
+	}
+	
+	/**
+	 * Test getType
+	 */
+	@Test
+	public void getTypeTest() {
+		Entity box = new Box(5, 4);
+		Entity background = new House(4, 3);
+			
+		assertTrue("box incorrect!", box.getType().toString().equals("BOX"));
+		assertTrue("background incorrect", background.getType().toString().equals("HOUSE"));
 	}
 }
