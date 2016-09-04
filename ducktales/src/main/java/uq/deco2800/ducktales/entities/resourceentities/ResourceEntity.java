dada65e518da19fd25611e9ecff9ed3425b393ad
Andrew Georgiou <gcgqldag@gmail.com>
2016-09-04 13:57:29 +1000

    Made some methods more efficient.

--- ducktales/src/main/java/uq/deco2800/ducktales/entities/resourceentities/ResourceEntity.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/entities/resourceentities/ResourceEntity.java
@@ -29,32 +29,57 @@ public abstract class ResourceEntity extends WorldEntity {
 	 */
 	public abstract boolean isPassable();
 	
+	/**
+	 * Called during constructor of a new Resource.
+	 * Has a 5% chance of spawning as a rare Resource with different visual and starting value.	
+	 * 
+	 * @param TYPES
+	 * 			   Array of appearances the Resource can take, 
+	 * 			   with the last one always being the rare option.
+	 * 
+	 * @return Returns the ResourceType that the Resource will use
+	 */
 	protected static ResourceType rare(ResourceType[] TYPES){
 		Random randomGenerator = new Random();
 		int randomInt = randomGenerator.nextInt(100);
 		if(randomInt < 5){
-			return TYPES[1];
+			return TYPES[TYPES.length-1];
 		} else{
 			return TYPES[0];
 		}
 	}
 	
+	/**
+	 * Sets the resource value of the Resource to x
+	 * 
+	 * @param x
+	 * 		   The new value of the Resource
+	 */	
 	protected void setValue(int x){
 		this.value = x;
 	}	
 	
-	protected void setResourceType(ResourceType R){
-		
-	}
-	
+	/**
+	 * @return The value of the Resource
+	 */		
 	public int getValue(){
 		return value;
 	}
-	
+	/**
+	 * Increase the value of the Resource by a specified amount
+	 * 
+	 * @param x
+	 * 		   The amount to add to the Resource value.
+	 */
 	public void increaseValue(int x){
 		value += x;
 	}
-
+	/**
+	 * Decrease the value of the Resource by a specified amount
+	 * 
+	 * @param x
+	 * 		   The amount to remove from the Resource value.
+	 */
 	public void decreaseValue(int x){
 		value -= x;
 	}
--- ducktales/src/main/java/uq/deco2800/ducktales/entities/resourceentities/Tree.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/entities/resourceentities/Tree.java
@@ -5,40 +5,30 @@ import java.util.concurrent.*;
 import static uq.deco2800.ducktales.resources.ResourceType.*;
 import uq.deco2800.ducktales.resources.ResourceType;
 
-public class Tree extends ResourceEntity implements Runnable{
-	private static ResourceType[] TYPES = {TREE_1, TREE_2, TREE_3};
-	
+public class Tree extends ResourceEntity{
+	private static ResourceType[] TYPES = {TREE_1, TREE_2, TREE_3};	
 	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
+	
 	public Tree(double x, double y) {		
 		super(x, y, 1, 1, rare(TYPES), defValue);
-		scheduler.scheduleAtFixedRate(createRunnable(this), 60, 60, TimeUnit.SECONDS);
+		scheduler.scheduleAtFixedRate(createRunnable(this), 5, 5, TimeUnit.SECONDS);
 		if(this.getType() == TYPES[1]){
 			this.setValue(200);
 		}
 	}
 	
-	private Runnable createRunnable(final Tree tree){	
+	private Runnable createRunnable(Tree tree){	
 		Runnable aRunnable = new Runnable(){
 			public void run(){
-				grow();
-				System.out.println(value);
+				tree.increaseValue(10);
+				//System.out.println(value);
 			}
 		};
 		return aRunnable;
 	}
 	
-	private void grow(){
-		this.increaseValue(10);
-	}
-	
 	@Override
-	public void run() {
-		
-	}
-
-	@Override
-	public void tick() {		
+	public void tick() {
 	}
 	
 	@Override
