ed1bf53ebcd69da6592d3765d4455006136e8b3f
Alison Collins <alison-collins@hotmail.com>
2016-09-09 14:08:25 +1000

    Changed implementation for duck god. Made it into an Abstract class, added to String method and a apply bonus method. Started writing implementing EarthGod but will need to talk to other teams to see how the bonus can be applied.

--- ducktales/src/main/java/uq/deco2800/ducktales/duckgods/DuckGods.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/duckgods/DuckGods.java
@@ -12,23 +12,32 @@ import uq.deco2800.ducktales.util.*;
  *
  */
 
-public class DuckGods {
+public abstract class DuckGods {
     
     // The name of the duck god selected.
     protected String duckName;
     // The bonus implemented by the duck god.
+    //We probably dont need this as every god class has its own specific bonus, having it stored as a string is redundant.
+    //probably better to hava toString method
     protected String bonus;
     // The image to be rendered of the duck god.
     protected ImageIcon image;
     
     
     
-
-    public void duckGod(String name, String bonus, ImageIcon image) {
+ 
+    public DuckGods(String name, String bonus, ImageIcon image) {
         this.duckName = name;
         this.bonus = bonus;
         this.image = image;
         //this.selectedDuckGod = ;
                 
     }
+    
+    public void applyBonus(){}
+    
+    public String toString(){
+        return "Abstract god with no power";
+    }
+    
 }
