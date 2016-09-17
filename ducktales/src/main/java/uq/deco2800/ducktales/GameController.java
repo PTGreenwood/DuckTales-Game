bed75f8c377916eac097108923d019ed0560cfa0
Matthew Lord <matty.j.lord@gmail.com>
2016-09-17 14:36:28 +1000

    Add in a new HUD (#2) to integrate with Day/Time (#1) .

--- ducktales/src/main/java/uq/deco2800/ducktales/GameController.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/GameController.java
@@ -273,17 +273,17 @@ public class GameController implements Initializable{
 
         try {
             // load the FXML
-            HBox timeDisplay = loader.load();
+            AnchorPane timeDisplay = loader.load();
 
             // Retrieve the controller
             timeManager = loader.getController();
 
             // Add the time display to the GUI
-            rootPane.getChildren().add(timeDisplay);
+            leftPane.getChildren().add(timeDisplay);
 
             // Position the time display
-            AnchorPane.setTopAnchor(timeDisplay, 0.0);
-            AnchorPane.setRightAnchor(timeDisplay, 400.0);
+            AnchorPane.setTopAnchor(timeDisplay, 20.0);
+            AnchorPane.setLeftAnchor(timeDisplay, 20.0);
 
         } catch (IOException e) {
             System.err.println("unable to load time display");
