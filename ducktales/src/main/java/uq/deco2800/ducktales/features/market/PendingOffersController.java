5b2967460b4d4b19cb61627272ab243cc410d2cd
MarkBelonogoff <mark.belonogoff@gmail.com>
2016-09-19 01:22:32 +1000

    Reverted back to previous commit to prevent the deletion of all the new marketplace GUI changes

---
+++ ducktales/src/main/java/uq/deco2800/ducktales/features/market/PendingOffersController.java
@@ -0,0 +1,94 @@
+package uq.deco2800.ducktales.features.market;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+public class PendingOffersController {
+	
+	private MarketManager marketManager;
+	
+	private static String OFFER_PANE = "/market/mppendingofferpane.fxml";
+	
+	private URL path;
+	
+	private static final Logger LOGGER = Logger.getLogger(
+			PendingOffersController.class.getName());
+	
+	@FXML
+	private GridPane yourOffersGridPane;
+	
+	public PendingOffersController() {
+		this.marketManager = MarketVistaNavigator.getMainController();
+	}
+	
+	@FXML
+	public void initialize() {
+		HashMap<MocTrade, MocTrade> userOffers 
+			= this.marketManager.getUserOffers();
+		
+		path = getClass().getResource(OFFER_PANE);
+		
+		int row = 0;
+		int column = 0;
+		for(MocTrade targetTrade: userOffers.keySet()) {
+			
+			MocTrade userOffer = userOffers.get(targetTrade);
+			
+			FXMLLoader loader = new FXMLLoader();
+			
+			try {
+				
+				loader.setLocation(path);
+				
+				Pane tradePane = (Pane) loader.load();
+			
+				Label itemNameLabel = 
+					(Label) tradePane.lookup("#itemNameLabel");
+				
+				itemNameLabel.setText(targetTrade.getItemName());
+				
+				Label quantityLabel = 
+						(Label) tradePane.lookup("#quantityLabel");
+				
+				quantityLabel.setText("Amount: " + 
+						targetTrade.getQuantity().toString());
+			
+				Label userLabel = (Label) tradePane.lookup("#userLabel");
+				userLabel.setText("User: " + targetTrade.getUserName());
+			
+			
+				GridPane.setRowIndex(tradePane, row);
+				GridPane.setColumnIndex(tradePane, column);
+			
+				yourOffersGridPane.getChildren().add(tradePane);
+           
+			} catch (IOException exception) {
+				LOGGER.log(Level.SEVERE, exception.toString(), exception);
+			
+			} catch(NullPointerException exception) {
+				
+				LOGGER.log(Level.SEVERE, exception.toString(), exception);
+				
+			}
+			
+			column++;
+			
+			if (column % 2 == 0) {
+				column = column % 2;
+				row++;
+			}
+			
+		}
+	}
+
+}
