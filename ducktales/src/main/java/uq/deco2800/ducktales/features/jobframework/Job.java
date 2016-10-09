18480b3fb3bafc80f495b35daf86e73d8e9c5cb3
Nicolas Turner <nicolas.turner@uqconnect.edu.au>
2016-10-09 17:08:01 +1000

    Tests now fit more within the "general" way of maging tests. Changed mothods for peon gathering %. Fixed math

--- ducktales/src/main/java/uq/deco2800/ducktales/features/jobframework/Job.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/features/jobframework/Job.java
@@ -8,157 +8,172 @@ import uq.deco2800.ducktales.features.entities.agententities.Peon;
  * @author Edward Liao (eduardlio on github and slack)
  */
 public abstract class Job {
-    
-    protected int requiredStrength;
-    protected int requiredIntelligence;
-    
-    protected int mentorStrength;
-    protected int mentorIntelligence;
-    
-    protected String jobName;
-    protected JobType jobtype;
-    /**
-     * Main constructor of the {@link Job} class.
-     * 
-     * @param requiredStrength
-     * @param requiredIntelligence
-     * @param mentorStrength
-     * @param mentorIntelligence 
-     * @param jobName 
-     */
-    public Job(int requiredStrength, int requiredIntelligence, 
-            int mentorStrength, int mentorIntelligence, String jobName, JobType jobtype) {
-        if (requiredStrength <= 0)
-            this.requiredStrength = 1;
-        if (requiredIntelligence <=0)
-            this.requiredIntelligence =1;
-        if (mentorStrength <= requiredStrength)
-            this.mentorStrength = requiredStrength+1;
-        if (mentorIntelligence <= requiredIntelligence)
-            this.mentorIntelligence = requiredIntelligence+1;
-        this.requiredStrength = requiredStrength;
-        this.requiredIntelligence = requiredIntelligence;
-        this.mentorStrength = mentorStrength;
-        this.mentorIntelligence = mentorIntelligence;
-        this.jobName = jobName;
-        this.jobtype = jobtype;
-    }
-    
-    /**
-     * Returns the name of the job
-     * @return jobName
-     */
-    public String getJobName(){
-        return jobName;
-    }
-    
-    /**
-     * Returns the requirements for the given job
-     * @return reqs, int[] of Required Values
-     */
-    public int [] getRequirements(){
-        int [] reqs = {this.getRequiredStrength(), this.getRequiredIntelligence()};
-        return reqs;
-    }
-    
-    /**
-     * Returns the required strength for the job
-     * @return requiredStrength
-     */
-    public int getRequiredStrength(){
-        return requiredStrength;
-    }
-    
-    /**
-     * Returns the required intelligence for the job
-     * @return requiredIntelligence
-     */
-    public int getRequiredIntelligence(){
-        return requiredIntelligence;
-    }
-    
-    /**
-     * Returns the requirements to become a Mentor in
-     * the given job
-     * @return reqs, int[] of Required Values
-     */
-    public int [] getMentorRequirements(){
-        int [] reqs = {this.getMentorStrength(), this.getMentorIntelligence()};
-        return reqs;      
-    }
-    
-    /**
-     * Returns the required strength to become
-     * a mentor in the job
-     * @return mentorStrength
-     */
-    public int getMentorStrength(){
-        return mentorStrength;
-    }
-    
-    /**
-     * Returns the required intelligence to become
-     * a mentor in the job
-     * @return mentorIntelligence;
-     */
-    public int getMentorIntelligence(){
-        return mentorIntelligence;
-    }
-    
-    /** 
-     * GOING TO NEED TO REVISE THIS PART
-     * to determine picking which peons to assign to the task
-     * 
-     * Determines overall how qualified a peon is for the job
-     * @param peon
-     * @return a double determining how qualified a peon is
-     *          for this job. 
-     */
-    public double qualifiedPercent(Peon peon){
-        if(!isQualified(peon))
-            return -1;
-        double str = peon.getStrength()/this.getRequiredStrength();
-        double intel = peon.getIntelligence()/this.getRequiredIntelligence();
-        double total = (str+intel)/2;
-        peon.setQualification(total);
-        return total;
-    }
-    
-    /**
-     * Defines whether or not the given peon is 
-     * qualified to do the job
-     * @param peon
-     * @return true iff peon is qualified, false otherwise
-     */
-    public boolean isQualified(Peon peon){
-        if (peon.getStrength() >= this.getRequiredStrength() &&
-                peon.getIntelligence() >= this.getRequiredIntelligence()){
-        	peon.setJob(jobName);
-            return true;
-        }
-        return false;
-    }
-    
-    /**
-     * Defines whether or not the given peon is 
-     * qualified to become a mentor in the job
-     * @param peon
-     * @return true iff peon is qualified and is already working, false otherwise
-     */
-    public boolean canBeMentor(Peon peon){
-        if (peon.getStrength() >= this.getMentorStrength()
-                && peon.getIntelligence()>=this.getMentorIntelligence()
-                ){
-            return true;
-        }
-        return false;
-    }
-    /**
-     * 
-     * @return 
-     */
-    @Override
-    public String toString(){
-        return this.jobName;
-    }
+
+	protected int requiredStrength;
+	protected int requiredIntelligence;
+
+	protected int mentorStrength;
+	protected int mentorIntelligence;
+
+	protected String jobName;
+	protected JobType jobtype;
+
+	/**
+	 * Main constructor of the {@link Job} class.
+	 * 
+	 * @param requiredStrength
+	 * @param requiredIntelligence
+	 * @param mentorStrength
+	 * @param mentorIntelligence
+	 * @param jobName
+	 */
+	public Job(int requiredStrength, int requiredIntelligence, int mentorStrength, int mentorIntelligence,
+			String jobName, JobType jobtype) {
+		if (requiredStrength <= 0)
+			this.requiredStrength = 1;
+		if (requiredIntelligence <= 0)
+			this.requiredIntelligence = 1;
+		if (mentorStrength <= requiredStrength)
+			this.mentorStrength = requiredStrength + 1;
+		if (mentorIntelligence <= requiredIntelligence)
+			this.mentorIntelligence = requiredIntelligence + 1;
+		this.requiredStrength = requiredStrength;
+		this.requiredIntelligence = requiredIntelligence;
+		this.mentorStrength = mentorStrength;
+		this.mentorIntelligence = mentorIntelligence;
+		this.jobName = jobName;
+		this.jobtype = jobtype;
+	}
+
+	/**
+	 * Returns the name of the job
+	 * 
+	 * @return jobName
+	 */
+	public String getJobName() {
+		return jobName;
+	}
+
+	/**
+	 * Redundant method? Nick
+	 * 
+	 * Returns the requirements for the given job
+	 * 
+	 * @return reqs, int[] of Required Values
+	 */
+	public int[] getRequirements() {
+		int[] reqs = { this.getRequiredStrength(), this.getRequiredIntelligence() };
+		return reqs;
+	}
+
+	/**
+	 * Returns the required strength for the job
+	 * 
+	 * @return requiredStrength
+	 */
+	public int getRequiredStrength() {
+		return requiredStrength;
+	}
+
+	/**
+	 * Returns the required intelligence for the job
+	 * 
+	 * @return requiredIntelligence
+	 */
+	public int getRequiredIntelligence() {
+		return requiredIntelligence;
+	}
+
+	/**
+	 * Returns the requirements to become a Mentor in the given job
+	 * 
+	 * @return reqs, int[] of Required Values
+	 */
+	public int[] getMentorRequirements() {
+		int[] reqs = { this.getMentorStrength(), this.getMentorIntelligence() };
+		return reqs;
+	}
+
+	/**
+	 * Returns the required strength to become a mentor in the job
+	 * 
+	 * @return mentorStrength
+	 */
+	public int getMentorStrength() {
+		return mentorStrength;
+	}
+
+	/**
+	 * Returns the required intelligence to become a mentor in the job
+	 * 
+	 * @return mentorIntelligence;
+	 */
+	public int getMentorIntelligence() {
+		return mentorIntelligence;
+	}
+
+	/**
+	 * Determines overall how qualified a peon is for the job
+	 * 
+	 * @param peon
+	 * @return a double that will be used for a multiplier on the resource
+	 *         gathering e.g. 1.25x
+	 */
+	public double qualifiedPercent(Peon peon) {
+		if (!isQualified(peon))
+			return -1;
+
+		if (this.getRequiredStrength() < this.getRequiredIntelligence()) {
+			double weight = this.getRequiredStrength() / this.getRequiredIntelligence();
+			double total = (peon.getStrength() / this.getRequiredStrength()) * weight
+					+ (peon.getIntelligence() / this.getRequiredIntelligence()) * (1 - weight);
+			peon.setQualification(total);
+			return total;
+		} else {
+			double weight = this.getRequiredIntelligence() / this.getRequiredStrength();
+			double total = (peon.getStrength() / this.getRequiredStrength()) * weight
+					+ (peon.getIntelligence() / this.getRequiredIntelligence()) * (1 - weight);
+			peon.setQualification(total);
+			return total;
+		}
+	}
+
+	/**
+	 * Defines whether or not the given peon is qualified to do the job
+	 * 
+	 * @param peon
+	 * @return true iff peon is qualified, false otherwise
+	 */
+	public boolean isQualified(Peon peon) {
+		if (peon.getStrength() >= this.getRequiredStrength()
+				&& peon.getIntelligence() >= this.getRequiredIntelligence()) {
+			peon.setJob(jobName);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Defines whether or not the given peon is qualified to become a mentor in
+	 * the job
+	 * 
+	 * @param peon
+	 * @return true iff peon is qualified and is already working, false
+	 *         otherwise
+	 */
+	public boolean canBeMentor(Peon peon) {
+		if (peon.getStrength() >= this.getMentorStrength() && peon.getIntelligence() >= this.getMentorIntelligence()) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@Override
+	public String toString() {
+		return this.jobName;
+	}
 }
--- ducktales/src/test/java/uq/deco2800/ducktales/jobframework/FarmerTest.java
+++ ducktales/src/test/java/uq/deco2800/ducktales/jobframework/FarmerTest.java
@@ -9,48 +9,83 @@ import static org.junit.Assert.*;
  * @author Nicolas Turner (TheFractal on github and Fractal on slack)
  */
 public class FarmerTest {
-    private Farmer farmer = new Farmer();
-    @Test
-    public void Test() {
-        Peon peon = new Peon(10, 10);
-        peon.setStrength(farmer.getRequiredStrength()+1);
-        peon.setIntelligence(farmer.getRequiredIntelligence()+1);
-        
-        assertEquals(peon.getJob(),"Jobless");
-        
-        boolean Qualified = farmer.isQualified(peon);
-        assertEquals(Qualified, farmer.isQualified(peon));
-        if (Qualified){
-            assertEquals(peon.applyForJob(farmer), "You're hired!");
-            assertEquals(peon.getJob(), "Farmer");
-        } else if ( peon.getStrength()<farmer.getRequiredStrength()
-                &&  peon.getIntelligence()<farmer.getRequiredIntelligence()){
-            assertEquals(peon.applyForJob(farmer),"Peon is not qualified in both aspects");
-        } else if (peon.getStrength()<farmer.getRequiredStrength()){
-            assertEquals(peon.applyForJob(farmer),"Peon strength is not high enough");
-        } else {
-            assertEquals(peon.applyForJob(farmer),"Peon intelligence is not high enough");
-        }
-    }
-    @Test
-    public void FarmerMentorTest(){
-        Peon peon = new Peon(10, 10);
-        peon.setStrength(farmer.getMentorStrength()-1);
-        peon.setIntelligence(farmer.getMentorIntelligence()-1);
-        peon.applyForJob(farmer);
-        // peon doesn't have the intelligence or strength to be mentor
-        assertFalse(farmer.canBeMentor(peon));
-        // peon has the intelligence but not the strength
-        peon.setIntelligence(farmer.getMentorIntelligence()+1);
-        assertFalse(farmer.canBeMentor(peon));
-        // peon has the strength but not the intelligence
-        peon.setStrength(farmer.getMentorStrength()+1);
-        peon.setIntelligence(farmer.getMentorIntelligence()-1);
-        assertFalse(farmer.canBeMentor(peon));
-        // peon has enough in both
-        peon.setStrength(farmer.getMentorStrength()+1);
-        peon.setIntelligence(farmer.getMentorIntelligence()+1);
-        assertTrue(farmer.canBeMentor(peon));
-    }
-        
+	private Farmer farmer = new Farmer();
+
+	@Test
+	public void qualificationTest() {
+		Peon peon = new Peon(10, 10);
+		peon.setStrength(farmer.getRequiredStrength() + 1);
+		peon.setIntelligence(farmer.getRequiredIntelligence() + 1);
+
+		assertEquals(peon.getJob(), "Jobless");
+
+		boolean Qualified = farmer.isQualified(peon);
+		assertEquals(Qualified, farmer.isQualified(peon));
+	}
+
+	public void applicationTest() {
+		Peon peon = new Peon(10, 10);
+		peon.setStrength(farmer.getRequiredStrength() + 1);
+		peon.setIntelligence(farmer.getRequiredIntelligence() + 1);
+
+		assertEquals(peon.applyForJob(farmer), "You're hired!");
+		assertEquals(peon.getJob(), "Farmer");
+	}
+
+	public void notQualifiedBothTest() {
+		Peon peon = new Peon(10, 10);
+		peon.setStrength(farmer.getRequiredStrength() - 1);
+		peon.setIntelligence(farmer.getRequiredIntelligence() - 1);
+
+		assertEquals(peon.applyForJob(farmer), "Peon is not qualified in both aspects");
+	}
+
+	public void notQualifiedStrengthTest() {
+		Peon peon = new Peon(10, 10);
+		peon.setStrength(farmer.getRequiredStrength() - 1);
+		peon.setIntelligence(farmer.getRequiredIntelligence() + 1);
+
+		assertEquals(peon.applyForJob(farmer), "Peon strength is not high enough");
+	}
+
+	public void notQualifiedIntelligenceTest() {
+		Peon peon = new Peon(10, 10);
+		peon.setStrength(farmer.getRequiredStrength() + 1);
+		peon.setIntelligence(farmer.getRequiredIntelligence() - 1);
+
+		assertEquals(peon.applyForJob(farmer), "Peon intelligence is not high enough");
+	}
+
+	@Test
+	public void notQualifiedBothMentorTest() {
+		Peon peon = new Peon(10, 10);
+		peon.setStrength(farmer.getMentorStrength() - 1);
+		peon.setIntelligence(farmer.getMentorIntelligence() - 1);
+		peon.applyForJob(farmer);
+
+		assertFalse(farmer.canBeMentor(peon));
+	}
+
+	public void notQualifiedStrengthMentorTest() {
+		Peon peon = new Peon(10, 10);
+		peon.setIntelligence(farmer.getMentorIntelligence() + 1);
+
+		assertFalse(farmer.canBeMentor(peon));
+	}
+
+	public void notQualifiedIntelligenceMentorTest() {
+		Peon peon = new Peon(10, 10);
+		peon.setStrength(farmer.getMentorStrength() + 1);
+		peon.setIntelligence(farmer.getMentorIntelligence() - 1);
+
+		assertFalse(farmer.canBeMentor(peon));
+	}
+
+	public void qualifiedBothMentorTest() {
+		Peon peon = new Peon(10, 10);
+		peon.setStrength(farmer.getMentorStrength() + 1);
+		peon.setIntelligence(farmer.getMentorIntelligence() + 1);
+
+		assertTrue(farmer.canBeMentor(peon));
+	}
 }
