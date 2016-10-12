72b2f31e66dc0839bc925e61f6d643ee5ba37483
UniqueLeaf <tylachen919@gmail.com>
2016-10-12 21:00:07 +1000

    create creature

---
+++ ducktales/src/main/java/uq/deco2800/ducktales/features/creatures/Cloudy.java
@@ -0,0 +1,20 @@
+package uq.deco2800.ducktales.features.creatures;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+/**
+ * Class for cloud creature
+ * 
+ * @author UniqueLeaf
+ * @author MattLeggy
+ * 
+ */
+ 
+ public class Cloudy extends Creatures {
+ 
+ public void loadImage {
+ }
+ 
+	public void spriteAnimation {
+  }
+ }
