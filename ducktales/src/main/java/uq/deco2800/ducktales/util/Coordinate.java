4d56444912ad59dd73f0dc3b26a680828a6370cd
Nicolas Turner <nturn13@live.com.au>
2016-10-12 13:16:09 +1000

    Added coordinate class and implemented the pathfinding methods

---
+++ ducktales/src/main/java/uq/deco2800/ducktales/util/Coordinate.java
@@ -0,0 +1,42 @@
+package uq.deco2800.ducktales.util;
+
+/**
+ * class will be used to store x and y coordinates
+ * 
+ * @author Nicolas Turner
+ */
+public class Coordinate {
+	int x = 0;
+	int y = 0;
+
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * returns the x and y values of the Coordinate in the form of a list
+	 * 
+	 */
+	public int[] getCoordinate() {
+		int[] coordinates = new int[2];
+		coordinates[0] = x;
+		coordinates[1] = y;
+		return coordinates;
+	}
+	
+	/**
+	 * returns the x value of the Coordinate in the form of an integer
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * returns the y value of the Coordinate in the form of an integer
+	 */
+	public int getY() {
+		return y;
+	}
+
+}
--- ducktales/src/main/java/uq/deco2800/ducktales/util/SpiralPathFinding.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/util/SpiralPathFinding.java
@@ -5,12 +5,75 @@
  */
 package uq.deco2800.ducktales.util;
 
+import java.util.ArrayList;
+import uq.deco2800.ducktales.World;
+import uq.deco2800.ducktales.util.Coordinate;
+
 /**
- * class will be used to find a free surrounding tile given
- * a specified tile. 
+ * class will be used to find a free surrounding tile given a specified tile.
  * 
- * @author Eduardo
+ * @author Eduardo and Nicolas Turner
  */
 public class SpiralPathFinding {
-    
+
+	/**
+	 * Generates a list of all possible coordinates that an object can be placed
+	 * at the front of a building
+	 *
+	 * @param x
+	 *            The x coordinate of the building
+	 * @param y
+	 *            The y coordinate of the building
+	 * @param sizex
+	 *            The length of the building
+	 * @param sizey
+	 *            The height of the building
+	 */
+	private static ArrayList<Coordinate> getAllPossibleCoordinates(int x, int y, int sizex, int sizey) {
+		int i;
+		int j;
+
+		ArrayList<Coordinate> possibleCoordinates = new ArrayList<>();
+
+		possibleCoordinates.add(new Coordinate(x + 1, y + 1));
+
+		for (i = 1; i <= sizex + 1; i++) {
+			possibleCoordinates.add(new Coordinate(x + 1 - i, y + 1));
+		}
+		for (j = 1; j <= sizey + 1; j++) {
+			possibleCoordinates.add(new Coordinate(x + 1, y + 1 - j));
+		}
+
+		return possibleCoordinates;
+	}
+
+	/**
+	 * Finds the next avaliable space that an object can be placed on the game
+	 * board
+	 *
+	 * @param x
+	 *            The x coordinate of the building
+	 * @param y
+	 *            The y coordinate of the building
+	 * @param sizex
+	 *            The length of the building
+	 * @param sizey
+	 *            The height of the building
+	 * @param world
+	 *            The Game world
+	 */
+	public Coordinate getFrontCoordinate(int x, int y, int sizex, int sizey, World world) {
+
+		ArrayList<Coordinate> allPossibleCoordinates = getAllPossibleCoordinates(x, y, sizex, sizey);
+
+		int i;
+		for (i = 0; i < allPossibleCoordinates.size(); i++) {
+			if (world.checkTileAvailability(allPossibleCoordinates.get(i).getX(), allPossibleCoordinates.get(i).getY(),
+					1, 1)) {
+				return allPossibleCoordinates.get(i);
+			}
+		}
+		return null;
+	}
+
 }
