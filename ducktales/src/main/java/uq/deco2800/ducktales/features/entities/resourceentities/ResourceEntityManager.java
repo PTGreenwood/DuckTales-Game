91b0409b6f04ad03d0c0ba8d6362cc3671b0c118
Damian Maher <dmaher145@gmail.com>
2016-10-13 22:08:43 +1000

    added resourceDrop method to resourceEntityManager class

--- ducktales/src/main/java/uq/deco2800/ducktales/features/entities/resourceentities/ResourceEntityManager.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/features/entities/resourceentities/ResourceEntityManager.java
@@ -1,6 +1,9 @@
 package uq.deco2800.ducktales.features.entities.resourceentities;
 
+import java.util.ArrayList;
+
 import uq.deco2800.ducktales.features.entities.EntityManager;
+import uq.deco2800.ducktales.resources.ResourceType;
 
 /**
  * Manager for all the resource entities in the game.
@@ -8,5 +11,20 @@ import uq.deco2800.ducktales.features.entities.EntityManager;
  * @author Damian Maher
  */
 public class ResourceEntityManager extends EntityManager {
-
+	
+    /**
+     * Add a resource drop at the location specified, this method mainly is used
+     * to drop an animal related resource when an animal dies (e.g. drop beef resource
+     * when a cow dies, etc)
+     *
+     * @param type
+     *          The type of resource to be added
+     * @param x
+     *          The x-coordinate of the resource in regards to the world size
+     * @param y
+     *          The y-coordinate of the resource in regards to the world size
+     */
+	public void dropResource(ResourceType type, int x, int y, ArrayList<ResourceType> registeredResources) {
+		
+	}
 }
