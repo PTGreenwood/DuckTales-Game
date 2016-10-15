e09805a92bb188c95d27c7ff2bc5fd39d542ed9f
Nicolas Turner <nicolas.turner@uqconnect.edu.au>
2016-10-15 12:27:45 +1000

    Changed the loadNameLists to use streams instead of file readers and added a test

--- ducktales/src/main/java/uq/deco2800/ducktales/resources/GeneralResourceRegister.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/resources/GeneralResourceRegister.java
@@ -14,82 +14,76 @@ import java.util.Random;
  * Created on 15/10/2016.
  */
 public class GeneralResourceRegister {
-    /** The lists of many first and last names to be used when generating peons */
-    private List<String> firstNames;
-    private List<String> lastNames;
+	/**
+	 * The lists of many first and last names to be used when generating peons
+	 */
+	private List<String> firstNames;
+	private List<String> lastNames;
 
-    private static GeneralResourceRegister ourInstance = new GeneralResourceRegister();
+	private static GeneralResourceRegister ourInstance = new GeneralResourceRegister();
 
-    public static GeneralResourceRegister getInstance() {
-        return ourInstance;
-    }
+	public static GeneralResourceRegister getInstance() {
+		return ourInstance;
+	}
 
-    private GeneralResourceRegister() {
-        firstNames = new ArrayList<>();
-        lastNames = new ArrayList<>();
+	//Made this public to allow a test to check if the flies load
+	public GeneralResourceRegister() {
+		firstNames = new ArrayList<>();
+		lastNames = new ArrayList<>();
 
-        loadNameLists();
-    }
+		loadNameLists();
+	}
 
-    /**
-     * Get a randomized name
-     *
-     * @return a name with randomized first and last names
-     */
-    public String getRandomName() {
-        Random random = new Random();
+	/**
+	 * Get a randomized name
+	 *
+	 * @return a name with randomized first and last names
+	 */
+	public String getRandomName() {
+		Random random = new Random();
 
-        int first = random.nextInt(5163);
-        int last = random.nextInt(5163);
+		int first = random.nextInt(5163);
+		int last = random.nextInt(5163);
 
+		return firstNames.get(first) + "" + lastNames.get(last);
+	}
 
-        
-        return firstNames.get(first) + "" + lastNames.get(last);
-    }
+	/**
+	 * Load the lists of random names into the names variables
+	 */
+	private void loadNameLists() {
+		try {
+			InputStream firstNameFileLocation = getClass().getClassLoader().getResourceAsStream("/peon/firstnames.txt");
+			InputStream lastNameFileLocation = getClass().getClassLoader().getResourceAsStream("/peon/lastnames.txt");
 
-    /**
-     * Load the lists of random names into the names variables
-     */
-    private void loadNameLists() {
-        try {
-            String firstNameFileLocation = getClass().getResource(
-                    "/peon/firstnames.txt").toString().substring(5);
-            String lastNameFileLocation = getClass().getResource(
-                    "/peon/lastnames.txt").toString().substring(5);
+			System.err.println("first names location: " + firstNameFileLocation);
 
-            System.err.println("first names location: " + firstNameFileLocation);
+			BufferedReader firstNamesReader = new BufferedReader(new InputStreamReader(firstNameFileLocation));
+			BufferedReader lastNamesReader = new BufferedReader(new InputStreamReader(lastNameFileLocation));
 
-            BufferedReader firstNamesReader = new BufferedReader(new FileReader(
-               new File(firstNameFileLocation)
-            ));
-            BufferedReader lastNamesReader = new BufferedReader(new FileReader(
-               new File(lastNameFileLocation)
-            ));
+			// Now start loading the first names and last names
+			String firstName = firstNamesReader.readLine();
+			while (firstName != null) {
+				firstNames.add(firstName);
+				firstName = firstNamesReader.readLine();
+			}
+			String lastName = lastNamesReader.readLine();
+			while (lastName != null) {
+				lastNames.add(lastName);
+				lastName = lastNamesReader.readLine();
+			}
 
-            // Now start loading the first names and last names
-            String firstName = firstNamesReader.readLine();
-            while (firstName != null) {
-                firstNames.add(firstName);
-                firstName = firstNamesReader.readLine();
-            }
-            String lastName = lastNamesReader.readLine();
-            while (lastName != null) {
-                lastNames.add(lastName);
-                lastName = lastNamesReader.readLine();
-            }
+			// Close the file streams
+			firstNamesReader.close();
+			lastNamesReader.close();
 
-            // Close the file streams
-            firstNamesReader.close();
-            lastNamesReader.close();
-
-        } catch (FileNotFoundException e) {
-            throw new GameSetupException("Cannot find files while trying to" +
-                    " load names in GeneralResourceRegister: " + e.getMessage());
-        } catch (IOException e) {
-            throw new RuntimeException("Error when trying to read the names" +
-                    "in the firstname.txt or lastname.txt files" +
-                    " in GeneralResourceRegister");
-        }
-    }
+		} catch (FileNotFoundException e) {
+			throw new GameSetupException(
+					"Cannot find files while trying to" + " load names in GeneralResourceRegister: " + e.getMessage());
+		} catch (IOException e) {
+			throw new RuntimeException("Error when trying to read the names"
+					+ "in the firstname.txt or lastname.txt files" + " in GeneralResourceRegister");
+		}
+	}
 
 }
---
+++ ducktales/src/test/java/uq/deco2800/ducktales/resources/GeneralResourceRegisterTest.java
@@ -0,0 +1,11 @@
+package uq.deco2800.ducktales.resources;
+
+public class GeneralResourceRegisterTest {
+
+	public void loadNameTest(){
+		//this would imply that the files load
+		GeneralResourceRegister test = new GeneralResourceRegister();
+		String name = test.getRandomName();
+		//System.out.print(name);
+	}
+}
