71e411ad0d076049737705f6a0188bcbaf051f14
Gabbi Hodge <gabbi.hodge@gmail.com>
2016-10-20 22:52:26 +1000

    comment out printlns from testing

--- ducktales/src/main/java/uq/deco2800/ducktales/rendering/animation/SpriteInterpolator.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/rendering/animation/SpriteInterpolator.java
@@ -61,8 +61,8 @@ public class SpriteInterpolator extends Interpolator {
         }
 
         // Return the right frame
-        System.out.println(spritesList.get(0).toString());
-        System.out.println(index);
+        //System.out.println(spritesList.get(0).toString());
+        //System.out.println(index);
         return spritesList.get(index);
     }
 
