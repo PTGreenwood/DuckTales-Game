385ae03868814a2a7becc9409fd0f4cc619b93f0
Matthew Lord <matty.j.lord@gmail.com>
2016-10-22 17:38:15 +1000

    Add extra commenting to weather classes.

--- ducktales/src/main/java/uq/deco2800/ducktales/features/weather/StormType.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/features/weather/StormType.java
@@ -1,5 +1,13 @@
 package uq.deco2800.ducktales.features.weather;
 
+/**
+ * 
+ * Storm type enumerator to be used to differentiate the different kinds of
+ * storms.
+ * 
+ * @author mattyleggy
+ *
+ */
 public enum StormType {
 	CYCLONE, TORNADO, LIGHTNING, THUNDER, WHIRLPOOL
 }
