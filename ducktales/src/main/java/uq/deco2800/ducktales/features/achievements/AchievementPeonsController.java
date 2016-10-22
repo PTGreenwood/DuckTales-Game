19bb52a91324128559613cbaec6d7ab73c7cf7f1
Justin16 <justin_kuhnel@hotmail.com>
2016-10-22 23:56:54 +1000

    added images for threat (created by @fhyd) as well as creating threat achievements and linking peon images to their corresponding achievement

--- ducktales/src/main/java/uq/deco2800/ducktales/features/achievements/AchievementPeonsController.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/features/achievements/AchievementPeonsController.java
@@ -7,7 +7,9 @@ import uq.deco2800.ducktales.World;
 public class AchievementPeonsController {
 	
 	
-	private Image peons1 = new Image("peon/basic_peon.png");
+	private Image peons1 = new Image("achievemnts/peon1.png");
+	private Image peons2 = new Image("achievemnts/peon2.png");
+	private Image peons3 = new Image("achievemnts/peon3.png");
 	private World world;
 	
 	
@@ -19,11 +21,11 @@ public class AchievementPeonsController {
 			achievementImage = peons1;
 		} else if (world.getNumberOfPeons() >= 25) {
 			
-			achievementImage = peons1;
+			achievementImage = peons2;
 		}
 	    else if (world.getNumberOfPeons() >= 100) {
 		
-		achievementImage = peons1;
+		achievementImage = peons3;
 	    }
 		
 		return achievementImage;
