723152582ecc1306268b7fdc0879f23bef1161d4
Zainab Alnaboud <zainab11bz@hotmail.com>
2016-10-23 12:09:29 +1000

    Added a ThreatFactoryTest class to test the ThreatFactory

---
+++ ducktales/src/test/java/uq/deco2800/ducktales/entities/ThreatFactoryTest.java
@@ -0,0 +1,41 @@
+package uq.deco2800.ducktales.entities;
+
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.util.ArrayList;
+
+import uq.deco2800.ducktales.World;
+import uq.deco2800.ducktales.features.entities.threats.Threat;
+import uq.deco2800.ducktales.features.entities.threats.Effect;
+import uq.deco2800.ducktales.features.entities.threats.Enemy;
+import uq.deco2800.ducktales.features.entities.threats.ThreatManager;
+import uq.deco2800.ducktales.features.entities.threats.ThreatFactory.EnemyType;
+import uq.deco2800.ducktales.features.entities.threats.ThreatFactory;
+
+import static org.junit.Assert.*;
+
+public class ThreatFactoryTest extends ApplicationTest{
+	
+	ThreatFactory threatFactory = new ThreatFactory();
+	
+	@Test
+	public void createLevel1EnemyTest(){
+		
+	}
+	
+	@Test
+	public void createLevel1EffectTest(){
+				
+	}
+	
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+	}
+	
+	
+
+}
