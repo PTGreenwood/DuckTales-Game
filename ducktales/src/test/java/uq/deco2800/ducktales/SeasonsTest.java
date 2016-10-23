8855221b66318cb6060591d294a4fc58a286dde3
Peter Greenwood <peter.greenwood92@live.com.au>
2016-10-23 23:24:57 +1000

    Added in SunSet and SunRise testing methods.

--- ducktales/src/test/java/uq/deco2800/ducktales/SeasonsTest.java
+++ ducktales/src/test/java/uq/deco2800/ducktales/SeasonsTest.java
@@ -131,6 +131,8 @@ public class SeasonsTest {
 		float correctSpHumidity = 8.0f;
 		int correctSpMaxTemperature = 25;
 		int correctSpMinTemperature = 5;
+		int correctSpNightFall = 4;
+		int correctSpDayBreak = 5;
 		float correctSpAtmosphericIceLevel = 1.0f;
 		float correctSpWindLevel = 1.0f;
 		float correctSpFireDanger = 2.8f;
@@ -138,6 +140,8 @@ public class SeasonsTest {
 		tm.getSeasonManager().getCurrentSeason().setHumidity(correctSpHumidity);
 		tm.getSeasonManager().getCurrentSeason().setMaximumTemperature(correctSpMaxTemperature);
 		tm.getSeasonManager().getCurrentSeason().setMinimumTemperature(correctSpMinTemperature);
+		tm.getSeasonManager().getCurrentSeason().setSunset(correctSpNightFall);
+		tm.getSeasonManager().getCurrentSeason().setSunrise(correctSpDayBreak);
 		tm.getSeasonManager().getCurrentSeason().setAtmosphericIceLevel(correctSpAtmosphericIceLevel);
 		tm.getSeasonManager().getCurrentSeason().setWindlevel(correctSpWindLevel);
 		tm.getSeasonManager().getCurrentSeason().setFireDanger(correctSpFireDanger);		
@@ -146,6 +150,8 @@ public class SeasonsTest {
 		assertEquals(correctSpHumidity, tm.getSeasonManager().getCurrentSeason().getHumidity(), 1.0f);
 		assertEquals(correctSpMaxTemperature, tm.getSeasonManager().getCurrentSeason().getMaxTemp());
 		assertEquals(correctSpMinTemperature, tm.getSeasonManager().getCurrentSeason().getMinTemp());
+		assertEquals(correctSpNightFall, tm.getSeasonManager().getCurrentSeason().getTimeNightFall());
+		assertEquals(correctSpDayBreak, tm.getSeasonManager().getCurrentSeason().getTimeDayBreak());
 		assertEquals(correctSpAtmosphericIceLevel, tm.getSeasonManager().getCurrentSeason().getAtmosphericIceLevel(), 1.0f);
 		assertEquals(correctSpWindLevel, tm.getSeasonManager().getCurrentSeason().getWindLevel(), 1.0f);
 		assertEquals(correctSpFireDanger, tm.getSeasonManager().getCurrentSeason().getFireDanger(), 1.0f);
@@ -157,6 +163,8 @@ public class SeasonsTest {
 		float correctSuHumidity = 2.7f;
 		int correctSuMaxTemperature = 26;
 		int correctSuMinTemperature = 16;
+		int correctSuNightFall = 5;
+		int correctSuDayBreak = 6;
 		float correctSuAtmosphericIceLevel = 0.0f;
 		float correctSuWindLevel = 3.0f;
 		float correctSuFireDanger = 1.9f;
@@ -164,6 +172,8 @@ public class SeasonsTest {
 		tm.getSeasonManager().getCurrentSeason().setHumidity(correctSuHumidity);
 		tm.getSeasonManager().getCurrentSeason().setMaximumTemperature(correctSuMaxTemperature);
 		tm.getSeasonManager().getCurrentSeason().setMinimumTemperature(correctSuMinTemperature);
+		tm.getSeasonManager().getCurrentSeason().setSunset(correctSuNightFall);
+		tm.getSeasonManager().getCurrentSeason().setSunrise(correctSuDayBreak);
 		tm.getSeasonManager().getCurrentSeason().setAtmosphericIceLevel(correctSuAtmosphericIceLevel);
 		tm.getSeasonManager().getCurrentSeason().setWindlevel(correctSuWindLevel);
 		tm.getSeasonManager().getCurrentSeason().setFireDanger(correctSuFireDanger);	
@@ -172,6 +182,8 @@ public class SeasonsTest {
 		assertEquals(correctSuHumidity, tm.getSeasonManager().getCurrentSeason().getHumidity(), 1.0f);
 		assertEquals(correctSuMaxTemperature, tm.getSeasonManager().getCurrentSeason().getMaxTemp());
 		assertEquals(correctSuMinTemperature, tm.getSeasonManager().getCurrentSeason().getMinTemp());
+		assertEquals(correctSuNightFall, tm.getSeasonManager().getCurrentSeason().getTimeNightFall());
+		assertEquals(correctSuDayBreak, tm.getSeasonManager().getCurrentSeason().getTimeDayBreak());
 		assertEquals(correctSuAtmosphericIceLevel, tm.getSeasonManager().getCurrentSeason().getAtmosphericIceLevel(), 1.0f);
 		assertEquals(correctSuWindLevel, tm.getSeasonManager().getCurrentSeason().getWindLevel(), 1.0f);
 		assertEquals(correctSuFireDanger, tm.getSeasonManager().getCurrentSeason().getFireDanger(), 1.0f);
@@ -182,6 +194,8 @@ public class SeasonsTest {
 		float correctAuHumidity = 1.8f;
 		int correctAuMaxTemperature = 18;
 		int correctAuMinTemperature = 8;
+		int correctAuNightFall = 15;
+		int correctAuDayBreak = 10;
 		float correctAuAtmosphericIceLevel = 0.0f;
 		float correctAuWindLevel = 1.7f;
 		float correctAuFireDanger = 0.6f;
@@ -189,6 +203,8 @@ public class SeasonsTest {
 		tm.getSeasonManager().getCurrentSeason().setHumidity(correctAuHumidity);
 		tm.getSeasonManager().getCurrentSeason().setMaximumTemperature(correctAuMaxTemperature);
 		tm.getSeasonManager().getCurrentSeason().setMinimumTemperature(correctAuMinTemperature);
+		tm.getSeasonManager().getCurrentSeason().setSunset(correctAuNightFall);
+		tm.getSeasonManager().getCurrentSeason().setSunrise(correctAuDayBreak);
 		tm.getSeasonManager().getCurrentSeason().setAtmosphericIceLevel(correctAuAtmosphericIceLevel);
 		tm.getSeasonManager().getCurrentSeason().setWindlevel(correctAuWindLevel);
 		tm.getSeasonManager().getCurrentSeason().setFireDanger(correctAuFireDanger);		
@@ -197,6 +213,8 @@ public class SeasonsTest {
 		assertEquals(correctAuHumidity, tm.getSeasonManager().getCurrentSeason().getHumidity(), 1.0f);
 		assertEquals(correctAuMaxTemperature, tm.getSeasonManager().getCurrentSeason().getMaxTemp());
 		assertEquals(correctAuMinTemperature, tm.getSeasonManager().getCurrentSeason().getMinTemp());
+		assertEquals(correctAuNightFall, tm.getSeasonManager().getCurrentSeason().getTimeNightFall());
+		assertEquals(correctAuDayBreak, tm.getSeasonManager().getCurrentSeason().getTimeDayBreak());
 		assertEquals(correctAuAtmosphericIceLevel, tm.getSeasonManager().getCurrentSeason().getAtmosphericIceLevel(), 1.0f);
 		assertEquals(correctAuWindLevel, tm.getSeasonManager().getCurrentSeason().getWindLevel(), 1.0f);
 		assertEquals(correctAuFireDanger, tm.getSeasonManager().getCurrentSeason().getFireDanger(), 1.0f);
@@ -207,6 +225,8 @@ public class SeasonsTest {
 		float correctWiHumidity = 0.5f;
 		int correctWiMaxTemperature = 11;
 		int correctWiMinTemperature = 0;
+		int correctWiNightFall = 12;
+		int correctWiDayBreak = 15;
 		float correctWiAtmosphericIceLevel = 4.8f;
 		float correctWiWindLevel = 1.0f;
 		float correctWiFireDanger = 0.0f;
@@ -214,6 +234,8 @@ public class SeasonsTest {
 		tm.getSeasonManager().getCurrentSeason().setHumidity(correctWiHumidity);
 		tm.getSeasonManager().getCurrentSeason().setMaximumTemperature(correctWiMaxTemperature);
 		tm.getSeasonManager().getCurrentSeason().setMinimumTemperature(correctWiMinTemperature);
+		tm.getSeasonManager().getCurrentSeason().setSunset(correctWiNightFall);
+		tm.getSeasonManager().getCurrentSeason().setSunrise(correctWiDayBreak);
 		tm.getSeasonManager().getCurrentSeason().setAtmosphericIceLevel(correctWiAtmosphericIceLevel);
 		tm.getSeasonManager().getCurrentSeason().setWindlevel(correctWiWindLevel);
 		tm.getSeasonManager().getCurrentSeason().setFireDanger(correctWiFireDanger);		
