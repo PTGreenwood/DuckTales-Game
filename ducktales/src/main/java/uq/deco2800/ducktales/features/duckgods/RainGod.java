f7193c01bae70f82c6bbfa4a4416b602337882be
Jake Wood <jakewood035@gmail.com>
2016-10-23 14:53:26 +1000

    Commenting out unused duckgods code.

--- ducktales/src/main/java/uq/deco2800/ducktales/features/duckgods/RainGod.java
+++ ducktales/src/main/java/uq/deco2800/ducktales/features/duckgods/RainGod.java
@@ -17,22 +17,22 @@ public class RainGod extends DuckGods {
 		super(name, bonus, image);
 	}
 
-	public void giveaward() {
-		if(isWorshiped){
-       /**Peon.speed= Peon.speed*2;
-		*need to change Speed visibility to public
-		*more award details
-		*/
-		}
-		
-	}
-	
-	public void givepenalty() {
-		if(isSelected && !isWorshiped){
-	  /**Peon.speed= Peon.speed*0.8;
-	   *need to change Speed visibility to public
-	   *more penalty detail
-	   */
-	}}  
+//	public void giveaward() {
+//		if(isWorshiped){
+//       /**Peon.speed= Peon.speed*2;
+//		*need to change Speed visibility to public
+//		*more award details
+//		*/
+//		}
+//		
+//	}
+//	
+//	public void givepenalty() {
+//		if(isSelected && !isWorshiped){
+//	  /**Peon.speed= Peon.speed*0.8;
+//	   *need to change Speed visibility to public
+//	   *more penalty detail
+//	   */
+//	}}  
         
 }
